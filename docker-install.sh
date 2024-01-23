6a0c61d344d196328fcc2ec2bfc9c4cef539cf95
Obruche Wilfred  Oghenechohwo <111436934+obrucheoghene@users.noreply.github.com>
2024-01-23 19:16:35 +0100

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -17,7 +17,19 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 
 # 5 Install Docker Engine:
 sudo apt update
+
+# if there is an error run this command
+gpg --keyserver keyserver.ubuntu.com --recv-key 7EA0A9C3F273FCD8
+gpg --export --armor 7EA0A9C3F273FCD8 | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# if no error continue from here
 sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+
+
+
 
 # 6 Start and Enable Docker:
 sudo systemctl start docker
